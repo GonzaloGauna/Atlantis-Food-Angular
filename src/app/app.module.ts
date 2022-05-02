1764b48ed3501b965efb52f086399201a1d0aefc
Gonzalo Gauna <79806700+GonzaloGauna@users.noreply.github.com>
2022-05-02 20:25:53 -0300

    Initializing proyect:     -header component added: unfinished         -nav subcomponent added: unfinished         -carousel subcomponent added: unfinished     -some styles added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './header/nav/nav.component';
+import { CarouselComponent } from './header/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
