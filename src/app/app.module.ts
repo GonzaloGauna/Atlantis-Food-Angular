20f4389922771b09cda58aaa44a757184b10179a
Gonzalo Gauna <79806700+GonzaloGauna@users.noreply.github.com>
2022-05-10 19:33:02 -0300

    Adding components files:     -about     -blogs     -home     -food     -gallery     -menu     -order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NavComponent } from './header/nav/nav.component';
-import { CarouselComponent } from './header/carousel/carousel.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/header/nav/nav.component';
+import { CarouselComponent } from './components/header/carousel/carousel.component';
+
+import { SwiperModule } from 'swiper/angular';
+import { AboutComponent } from './components/about/about.component';
+import { FoodComponent } from './components/food/food.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { OrderComponent } from './components/order/order.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
-    CarouselComponent
+    CarouselComponent,
+    AboutComponent,
+    FoodComponent,
+    GalleryComponent,
+    HomeComponent,
+    MenuComponent,
+    OrderComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
